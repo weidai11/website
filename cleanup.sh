e40865960ba473eeae5e712f968de90a6f1f699e
Jeffrey Walton <noloader@gmail.com>
2017-01-31 02:50:52 -0500

    Update homepage with additional algorithms Whitespace in cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,51 +8,52 @@ SED_OPTS=
 
 IS_DARWIN=$(uname -s | grep -i -c darwin)
 if [[ "$IS_DARWIN" -ne 0 ]]; then
-	SED_OPTS=(-i "")
+    SED_OPTS=(-i "")
 else
-	SED_OPTS=(-i)
+    SED_OPTS=(-i)
 fi
 
 HTML_TIDY=$(which tidy)
 if [[ -z "$HTML_TIDY" ]]; then
-	HTML_TIDY=$(which htmltidy)
+    HTML_TIDY=$(which htmltidy)
 fi
 
 if [[ -z "$HTML_TIDY" ]]; then
-	echo "ERROR: could not locate HTML Tidy"
-	[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    echo "ERROR: could not locate HTML Tidy"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
 fi
 
 UNIX2DOS=$(which unix2dos)
 if [[ -z "$UNIX2DOS" ]]; then
-	echo "ERROR: could not locate unix2dos"
-	[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+    echo "ERROR: could not locate unix2dos"
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
 fi
 
 # Cleanup HTML files
 for file in *.html
 do
-	echo "**************** $file ****************"
+    echo "**************** $file ****************"
 
-	echo "tidy: processing file $file..."
-	"$HTML_TIDY" --quiet yes --output-bom no --indent auto --wrap 90 -m "$file"
+    echo "tidy: processing file $file..."
+    "$HTML_TIDY" --quiet yes --output-bom no --indent auto --wrap 90 -m "$file"
 
-	echo "sed: processing file $file..."
+    echo "sed: processing file $file..."
 
-	# Delete trailing whitespace
-	"$SED" "${SED_OPTS[@]}" -e's/[[:space:]]*$//' "$file"
+    # Delete trailing whitespace
+    "$SED" "${SED_OPTS[@]}" -e's/[[:space:]]*$//' "$file"
 
-	# Delete the generator markup tag
-	"$SED" "${SED_OPTS[@]}" -e'/<meta name="generator"/d' "$file"
-	"$SED" "${SED_OPTS[@]}" -e'/"HTML Tidy/d' "$file"
+    # Delete the generator markup tag
+    "$SED" "${SED_OPTS[@]}" -e'/<meta name="generator".*/d' "$file"
+    "$SED" "${SED_OPTS[@]}" -e'/"HTML Tidy.*/d' "$file"
+    "$SED" "${SED_OPTS[@]}" -e'/"*see www.w3.org*/d' "$file"
 
-	# Fix change from UTF-8 to ASCII
-        "$SED" "${SED_OPTS[@]}" -e's/charset=us-ascii/charset=utf-8/g' "$file"
+    # Fix change from UTF-8 to ASCII
+    "$SED" "${SED_OPTS[@]}" -e's/charset=us-ascii/charset=utf-8/g' "$file"
 
-	# Fix CRLF endings after sed
-	unix2dos "$file"
-	
-	echo
+    # Fix CRLF endings after sed
+    unix2dos "$file"
+
+    echo
 done
 
 # Delete the generator markup tag
