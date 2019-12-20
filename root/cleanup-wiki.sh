dbed61fbabe96bb3affc92fd49589b1ea8d1d571
Jeffrey Walton <noloader@gmail.com>
2019-12-19 23:16:48 -0500

    Update cleanup script

--- root/cleanup-wiki.sh
+++ root/cleanup-wiki.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
+# Be careful with cleanupSpam.php. The domain argument is
+# the spam domain, and not localhost or cryptopp.com.
+
 wiki_root=/var/www/html/w
 wiki_user=$(grep 'wgDBuser' "$wiki_root/LocalSettings.php" | cut -d '"' -f 2)
 wiki_pass=$(grep 'wgDBpassword' "$wiki_root/LocalSettings.php" | cut -d '"' -f 2)
 
+# https://phabricator.wikimedia.org/T172060
+# "--config=$wiki_root/LocalSettings.php"
 script_args=(
     "--dbuser=$wiki_user"
-    "--dbpass=$wiki_pass"    
+    "--dbpass=$wiki_pass"
+    "--server=https://www.cryptopp.com/wiki/"
 )
-
-# "--config=$wiki_root/LocalSettings.php"
-
-php "$wiki_root/maintenance/cleanupUsersWithNoId.php" "${script_args[@]}" --prefix="wikicryptopp_" --force
-php "$wiki_root/maintenance/cleanupUsersWithNoId.php" "${script_args[@]}" --prefix="wikilounge_" --force
 
 php "$wiki_root/maintenance/deleteArchivedFiles.php" "${script_args[@]}" --delete
 
@@ -29,3 +30,20 @@ php "$wiki_root/maintenance/deleteOldRevisions.php" "${script_args[@]}"
 php "$wiki_root/maintenance/deleteOrphanedRevisions.php" "${script_args[@]}"
 
 php "$wiki_root/maintenance/deleteSelfExternals.php" "${script_args[@]}"
+
+php "$wiki_root/maintenance/cleanupUsersWithNoId.php" "${script_args[@]}" --prefix="wikicryptopp_" --force
+php "$wiki_root/maintenance/cleanupUsersWithNoId.php" "${script_args[@]}" --prefix="wikilounge_" --force
+
+php "$wiki_root/maintenance/cleanupAncientTables.php" "${script_args[@]}" --force
+php "$wiki_root/maintenance/cleanupBlocks.php" "${script_args[@]}"
+php "$wiki_root/maintenance/cleanupCaps.php" "${script_args[@]}"
+php "$wiki_root/maintenance/cleanupEmptyCategories.php" "${script_args[@]}"
+php "$wiki_root/maintenance/cleanupImages.php" "${script_args[@]}"
+php "$wiki_root/maintenance/cleanupInvalidDbKeys.php" "${script_args[@]}"
+php "$wiki_root/maintenance/cleanupRemovedModules.php" "${script_args[@]}"
+php "$wiki_root/maintenance/cleanupRevActorPage.php" "${script_args[@]}" --force
+php "$wiki_root/maintenance/cleanupTitles.php" "${script_args[@]}"
+php "$wiki_root/maintenance/cleanupUploadStash.php" "${script_args[@]}"
+php "$wiki_root/maintenance/cleanupWatchlist.php" "${script_args[@]}" --fix
+php "$wiki_root/maintenance/cleanupPreferences.php" "${script_args[@]}" --hidden
+php "$wiki_root/maintenance/cleanupPreferences.php" "${script_args[@]}" --unknown
