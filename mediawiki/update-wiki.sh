dfe1c6d39310ab08352baa2bdeebf11841f0e232
Jeffrey Walton <noloader@gmail.com>
2020-12-14 00:34:12 -0500

    Update update-wiki.sh script

--- mediawiki/update-wiki.sh
+++ mediawiki/update-wiki.sh
@@ -8,8 +8,13 @@
 # the Apache service. update.php is important and it must be run anytime
 # a change occurs.
 #
-# The script is located in the website directory, which is /var/www/html/.
+# We have to use the safer form of the find command because some skins and
+# extensions use whitespace in the filenames of some of their files.
+#
+# The script is located in the wiki directory, which is /var/www/html/w.
 # We should probably schedule this script as a cron job.
+#
+# This script takes about 10 minutes to run.
 
 THIS_DIR=$(pwd)
 function finish {
@@ -31,48 +36,48 @@ PHP_DIR=/opt/rh/rh-php72/root/usr/bin
 LOG_DIR="/var/log"
 
 if [[ ! -d "${WIKI_DIR}" ]]; then
-    echo "WIKI_DIR is not vaild."
+    echo "WIKI_DIR is not valid."
     exit 1
 fi
 
 if [[ ! -d "${PHP_DIR}" ]]; then
-    echo "PHP_DIR is not vaild."
+    echo "PHP_DIR is not valid."
     exit 1
 fi
 
 # This finds directories check'd out from Git and updates them. 
 # It works surprisingly well. There has only been a couple of
 # minor problems.
-for dir in $(find "$WIKI_DIR/skins" -name '.git' 2>/dev/null); do
+IFS= find "$WIKI_DIR/skins" -type d -name '.git' -print | while read -r dir
+do
     cd "$dir/.."
     echo "Updating ${dir::-4}"
     git reset --hard HEAD && git pull && \
-    git checkout -f "$WIKI_REL" && git pull
+      git checkout -f "$WIKI_REL" && git pull
 done
 
-for dir in $(find "$WIKI_DIR/extensions" -name '.git' 2>/dev/null); do
+IFS= find "$WIKI_DIR/extensions" -type d -name '.git' -print | while read -r dir
+do
     cd "$dir/.."
     echo "Updating ${dir::-4}"
     git reset --hard HEAD && git pull && \
-    git checkout -f "$WIKI_REL" && git pull
+      git checkout -f "$WIKI_REL" && git pull
 done
 
 # Remove all test frameworks
-for dir in $(find "$WIKI_DIR" -iname 'test*' 2>/dev/null); do
+IFS= find "$WIKI_DIR" -iname 'test*' -print | while read -r dir
+do
     rm -rf "$dir" 2>/dev/null
 done
 
 # And benchmarks
-for dir in $(find "$WIKI_DIR" -iname 'benchmark*' 2>/dev/null); do
+IFS= find "$WIKI_DIR" -iname 'benchmark*' -print | while read -r dir
+do
     rm -rf "$dir" 2>/dev/null
 done
 
-if [[ -f "$WIKI_DIR/extensions/SyntaxHighlight/pygments/pygmentize" ]]; then
-    chmod ug+x "$WIKI_DIR/extensions/SyntaxHighlight/pygments/pygmentize"
-fi
-
+echo "Creating MediaWiki sitemap"
 if [[ -f "$WIKI_DIR/create-sitemap.sh" ]]; then
-    echo "Creating MediaWiki sitemap"
     rm -rf "$WIKI_DIR/sitemap"
     bash "$WIKI_DIR/create-sitemap.sh" 1>/dev/null
 fi
@@ -83,59 +88,86 @@ fi
 
 echo "Fixing MediaWiki permissions"
 chown -R root:apache "$WIKI_DIR/"
-chmod -R u+rw,g+r,g-w,o-rwx "$WIKI_DIR/"
+IFS= find "$WIKI_DIR" -type d -print | while read -r dir
+do
+    chmod u=rwx,g=rx,o= "$dir"
+done
+IFS= find "$WIKI_DIR" -type f -print | while read -r file
+do
+    chmod u=rw,g=r,o= "$file"
+done
 
 # Make Python and PHP executable
 echo "Fixing Python and PHP permissions"
-for file in $(find "$WIKI_DIR" -type f -name '*.py' 2>/dev/null); do
-    if [[ ! -f "$file" ]]; then continue; fi
-    chmod u+rwx,g+rx,g-w,o-rwx "$file"
+IFS= find "$WIKI_DIR" -type f -name '*.py' -print | while read -r file
+do
+    chmod u=rwx,g=rx,o= "$file"
 done
-for file in $(find "$WIKI_DIR" -type f -name '*.php' 2>/dev/null); do
-    if [[ ! -f "$file" ]]; then continue; fi
-    chmod u+rwx,g+rx,g-w,o-rwx "$file"
+IFS= find "$WIKI_DIR" -type f -name '*.php' -print | while read -r file
+do
+    chmod u=rwx,g=rx,o= "$file"
 done
-for file in $(find "$WIKI_DIR" -type f -name '*.sh' 2>/dev/null); do
-    if [[ ! -f "$file" ]]; then continue; fi
-    chmod u+rwx,g+rx,g-w,o-rwx "$file"
+IFS= find "$WIKI_DIR" -type f -name '*.sh' -print | while read -r file
+do
+    chmod u=rwx,g=rx,o= "$file"
 done
 
 # Images/ must be writable by apache group
 echo "Fixing MediaWiki images/ permissions"
-for dir in $(find "$WIKI_DIR/images" -type d 2>/dev/null); do
-    if [[ ! -d "$dir" ]]; then continue; fi
-    chmod ug+rwx,o-rwx "$dir"
+IFS= find "$WIKI_DIR/images" -type d | while read -r dir
+do
+    chmod ug=rwx,o= "$dir"
 done
-for file in $(find "$WIKI_DIR/images" -type f 2>/dev/null); do
-    if [[ ! -f "$file" ]]; then continue; fi
-    chmod ug+rw,ug-x,o-rwx "$file"
+IFS= find "$WIKI_DIR/images" -type f | while read -r file
+do
+    chmod ug=rw,o= "$file"
 done
 
 echo "Fixing Apache data permissions"
-for dir in "/var/lib/pear/" "/var/lib/php/"; do
-    if [[ ! -d "$dir" ]]; then continue; fi
-    chown -R apache:apache "$dir"
-    chmod -R ug+rwx,o-rwx "$dir"
-done
+if [[ -d "/var/lib/pear" ]]
+then
+    chown -R apache:apache "/var/lib/pear"
+    IFS= find "/var/lib/pear" -type d | while read -r dir
+    do
+        chmod ug=rwx,o= "$dir"
+    done
+    IFS= find "/var/lib/pear" -type f | while read -r file
+    do
+        chmod ug=rw,o= "$file"
+    done
+fi
+
+if [[ -d "/var/lib/php" ]]
+then
+    chown -R apache:apache "/var/lib/php"
+    IFS= find "/var/lib/php" -type d | while read -r dir
+    do
+        chmod ug=rwx,o= "$dir"
+    done
+    IFS= find "/var/lib/php" -type f | while read -r file
+    do
+        chmod ug=rw,o= "$file"
+    done
+fi
 
 echo "Fixing Apache logging permissions"
-for dir in $(find "$LOG_DIR" -type d -name 'httpd*' 2>/dev/null); do
-    if [[ ! -d "$dir" ]]; then continue; fi
+IFS= find "$LOG_DIR" -type d -name 'httpd*' | while read -r dir
+do
     chown root:apache "$dir"
-    chmod ug+rwx,o-rwx "$dir"
+    chmod ug=rwx,o= "$dir"
 done
-for file in $(find "$LOG_DIR/httpd*" -type f -name '*log*' 2>/dev/null); do
-    if [[ ! -f "$file" ]]; then continue; fi
+IFS= find "$LOG_DIR/httpd*" -type f -name '*log*' | while read -r file
+do
     chown root:apache "$file"
-    chmod ug+rw,ug-x,o-rwx "$file"
+    chmod ug=rw,o= "$file"
 done
 
 echo "Fixing MariaDB logging permissions"
 chown mysql:mysql "$LOG_DIR/mariadb"
-for file in $(find "$LOG_DIR/mariadb" -type f -name '*log*' 2>/dev/null); do
-    if [[ ! -f "$file" ]]; then continue; fi
+IFS= find "$LOG_DIR/mariadb" -type f -name '*log*' | while read -r file
+do
     chown mysql:mysql "$file"
-    chmod ug+rw,ug-x,o-rwx "$file"
+    chmod ug=rw,o= "$file"
 done
 
 # Make sure MySQL is running for update.php. It is a chronic
