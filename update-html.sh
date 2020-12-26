582219dff7295313a9c7fbb7be1fae21471d0df6
Jeffrey Walton <noloader@gmail.com>
2020-12-26 14:55:20 -0500

    Update update-html.sh script

--- update-html.sh
+++ update-html.sh
@@ -41,14 +41,15 @@ if [ "${count}" -ne 0 ]; then
     mv *.ico "${www_directory}"
 fi
 
-count=$(ls -1 cryptopp*.zip 2>/dev/null | wc -l)
+# Early Crypto++ filenames were crypto23.zip, etc.
+count=$(ls -1 crypto*.zip 2>/dev/null | wc -l)
 if [ "${count}" -ne 0 ]; then
-    mv cryptopp*.zip "${www_directory}"
+    mv crypto*.zip "${www_directory}"
 fi
 
-count=$(ls -1 cryptopp*.sig 2>/dev/null | wc -l)
+count=$(ls -1 crypto*.sig 2>/dev/null | wc -l)
 if [ "${count}" -ne 0 ]; then
-    mv cryptopp*.sig "${www_directory}"
+    mv crypto*.sig "${www_directory}"
 fi
 
 # Ownership
@@ -70,6 +71,18 @@ do
     else
         chmod u=rw,g=r,o= "${file}"
     fi
+done
+
+# Finally, set the date/time on the ZIP files
+# This piece is the update-zip.sh script
+IFS= find "${www_directory}" -maxdepth 1 -type f -name '*.zip' -print | while read -r file
+do
+    # This fetches the newest timestamp from the ZIP file. It is in format '01-17-1998 20:19'
+    original=$(unzip -l "${file}" 2>&1 | head -n -2 | tail -n +4 | cut -b 12-27 | sort -r | uniq | head -n 1)
+    # '01-17-1998 20:19' -> '1998-01-17 20:19'
+    original=$(echo ${original} | sed 's/\([^-]*-[^-]*\)-\(....\)/\2-\1/')
+    # echo "Setting ${file} to ${original}"
+    touch -d "${original}" "${file}"
 done
 
 exit 0
