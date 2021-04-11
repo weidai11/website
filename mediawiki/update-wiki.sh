69a877dac3fa55b0a2ceb2274139db492706b07f
Jeffrey Walton <noloader@gmail.com>
2021-04-11 19:10:08 -0400

    Update upgrade-wiki.sh script

--- mediawiki/update-wiki.sh
+++ mediawiki/update-wiki.sh
@@ -22,6 +22,10 @@ wiki_dir="/var/www/html/w"
 wiki_rel=REL1_35
 php_bin=/usr/bin/php
 
+# Pretty print
+green_color='\033[0;32m'
+no_color='\033[0m'
+
 # Privileges? Exit 0 to keep things moving along
 # Errors will be printed to the terminal
 if [[ $(id -u) != "0" ]]; then
@@ -90,17 +94,41 @@ chown -R ${user_group} "${html_dir}"
 IFS= find "${wiki_dir}/skins" -type d -name '.git' -print | while read -r dir
 do
     cd "$dir/.." || continue
-    echo "Updating ${dir::-4}"
-    git reset --hard HEAD && git pull && \
-      git checkout -f "${wiki_rel}" && git pull
+    skin="$(basename ${dir::-4})"
+    echo -e "${green_color}Updating ${skin}${no_color}"
+
+    if git branch -a 2>/dev/null | grep -q "${wiki_rel}"
+    then
+        git fetch origin && git reset --hard "origin/${wiki_rel}" && \
+          git checkout -f "${wiki_rel}" && git pull && git clean -xdf
+    else
+        # Some GitHubs don't follow Mediawiki conventions.
+        # They lack a branch like REL1_32, REL1_35, etc.
+        git reset --hard origin && git pull && git clean -xdf
+    fi
+
+    # Cleanup
+    git fetch --prune >/dev/null 2>&1
 done
 
 IFS= find "${wiki_dir}/extensions" -type d -name '.git' -print | while read -r dir
 do
     cd "$dir/.." || continue
-    echo "Updating ${dir::-4}"
-    git reset --hard HEAD && git pull && \
-      git checkout -f "${wiki_rel}" && git pull
+    extension="$(basename ${dir::-4})"
+    echo -e "${green_color}Updating ${extension}${no_color}"
+
+    if git branch -a 2>/dev/null | grep -q "${wiki_rel}"
+    then
+        git fetch origin && git reset --hard "origin/${wiki_rel}" && \
+          git checkout -f "${wiki_rel}" && git pull && git clean -xdf
+    else
+        # Some GitHubs don't follow Mediawiki conventions.
+        # They lack a branch like REL1_32, REL1_35, etc.
+        git reset --hard origin && git pull && git clean -xdf
+    fi
+
+    # Cleanup
+    git fetch --prune >/dev/null 2>&1
 done
 
 # Set ownership of the Mediawiki files. The git checkout may upset ownership.
@@ -137,7 +165,7 @@ do
     rm -rf "$dir" 2>/dev/null
 done
 
-echo "Creating MediaWiki sitemap"
+echo -e "${green_color}Creating MediaWiki sitemap${no_color}"
 if [[ -f "${wiki_dir}/create-sitemap.sh" ]]; then
     rm -rf "${wiki_dir}/sitemap"
     bash "${wiki_dir}/create-sitemap.sh" 1>/dev/null
@@ -150,7 +178,7 @@ fi
 # We would like to skip images/ here, but find is too sideways.
 # images/ gets different permissions, and find's -prune does not
 # seem to work as expected.
-echo "Setting MediaWiki permissions"
+echo -e "${green_color}Setting MediaWiki permissions${no_color}"
 IFS= find "${wiki_dir}" -type d -print | while read -r dir
 do
     chmod u=rwx,g=rx,o= "$dir"
@@ -162,7 +190,7 @@ done
 
 # images/ must be writable by Apache. This is the upload
 # directory, and the directory where thumbnails are created.
-echo "Setting MediaWiki images/ permissions"
+echo -e "${green_color}Setting MediaWiki images/ permissions${no_color}"
 IFS= find "${wiki_dir}/images" -type d | while read -r dir
 do
     chmod ug=rwx,o= "$dir"
@@ -173,7 +201,7 @@ do
 done
 
 # Make Python, PHP and friends executable
-echo "Setting Executable file permissions"
+echo -e "${green_color}Setting Executable file permissions${no_color}"
 IFS= find "${wiki_dir}" -type f -print | while read -r file
 do
     if file -b "${file}" | grep -q -E 'executable|script';
@@ -186,7 +214,7 @@ done
 
 # Make sure MySQL is running for update.php. It is a chronic
 # problem because the Linux OOM killer targets mysqld.
-echo "Restarting MySQL service"
+echo -e "${green_color}Restarting MySQL service${no_color}"
 if ! systemctl restart ${mysql_service}; then
     echo "Restart failed. Sleeping for 3 seconds"
     sleep 3
@@ -196,10 +224,10 @@ if ! systemctl restart ${mysql_service}; then
 fi
 
 # Always run update script per https://www.mediawiki.org/wiki/Manual:Update.php
-echo "Running update.php"
+echo -e "${green_color}Running update.php${no_color}"
 "${php_bin}" "${wiki_dir}/maintenance/update.php" --quick --server="https://www.cryptopp.com/wiki"
 
-echo "Restarting Apache service"
+echo -e "${green_color}Restarting Apache service${no_color}"
 if ! systemctl restart ${apache_service}; then
     echo "Restart failed. Sleeping for 3 seconds"
     sleep 3
@@ -209,7 +237,7 @@ if ! systemctl restart ${apache_service}; then
 fi
 
 # Cleanup backup files
-echo "Cleaning backup files"
+echo -e "${green_color}Cleaning backup files${no_color}"
 find /var/www -name '*~' -exec rm {} \;
 find /opt -name '*~' -exec rm {} \;
 find /etc -name '*~' -exec rm {} \;
